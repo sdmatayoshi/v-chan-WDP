3fad26c307ce6c25cc32367409dd100b2ca03bdc
sdmatayoshi <sdmatayoshi@gmail.com>
2024-08-12 21:04:26 -0300

    convert to maven project

--- src/main/java/servlet.java
+++ src/main/java/servlet.java
@@ -1,24 +1,3 @@
-/*import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-@WebServlet("/servlet")
-public class servlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-    public servlet() {
-        super();
-    }
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-}*/
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
